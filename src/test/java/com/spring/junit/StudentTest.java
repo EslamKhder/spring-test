7c78dfba9461f34198310e08751eb51eb5d4ff0e
Eslam Khder <eslamkhder81@gmail.com>
2021-07-09 21:53:17 +0200

    @BeforeEach @BeforeAll

--- src/test/java/com/spring/junit/StudentTest.java
+++ src/test/java/com/spring/junit/StudentTest.java
@@ -1,19 +1,32 @@
 package com.spring.junit;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
 class StudentTest {
 
+    private Student student;
+    @BeforeAll
+    static void beforeAllFunction() {
+        System.out.println("@BeforeAll..........");
+    }
+
+    @BeforeEach
+    void setUpFunction() {
+        student = new Student();
+        System.out.println("@BeforeEach..........");
+    }
+
     @Test
     void getName() {
-        Student student = new Student();
         System.out.println(student.getName());
     }
 
     @Test
     void getAge() {
-        Student student = new Student();
         System.out.println(student.getAge());
     }
 }
