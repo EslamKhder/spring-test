036c7daa101ca4e407f896f426042c8e8e9bcae9
Eslam Khder <eslamkhder81@gmail.com>
2021-07-10 18:12:55 +0200

    assertAll with assertEquals

---
+++ src/main/java/com/spring/junit/Person.java
@@ -0,0 +1,45 @@
+package com.spring.junit;
+
+public class Person {
+
+    private String fName = "Eslam";
+    private String lName = "Khder";
+    private String phone1 = "01113903660";
+    private String phone2 = "01255582855";
+
+
+    public Person() {
+    }
+
+    public String getFName() {
+        return fName;
+    }
+
+    public void setFName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getLName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getPhone1() {
+        return phone1;
+    }
+
+    public void setPhone1(String phone1) {
+        this.phone1 = phone1;
+    }
+
+    public String getPhone2() {
+        return phone2;
+    }
+
+    public void setPhone2(String phone2) {
+        this.phone2 = phone2;
+    }
+}
--- src/test/java/com/spring/junit/StudentTest.java
+++ src/test/java/com/spring/junit/StudentTest.java
@@ -5,6 +5,33 @@ import org.junit.jupiter.api.*;
 class StudentTest {
 
     @Test
+    void personTest(){
+        Person person = new Person();
+        /*Assertions.assertAll
+                ("Student Data",
+                        () -> Assertions.assertEquals(person.getFName(),"Eslam","F Name not Matches"),
+                        () -> Assertions.assertEquals(person.getLName(),"Khder","L Name  not Matches"),
+                        () -> Assertions.assertEquals(person.getPhone1(),"01113903660","Phone not Matches"),
+                        () -> Assertions.assertEquals(person.getPhone2(),"01255582855","Phone  not Matches")
+                );*/
+        Assertions.assertAll("Person Data",
+                () -> Assertions.assertAll
+                        ("Person Names",
+                                () -> Assertions.assertEquals(person.getFName(),"Eslamm","F Name not Matches"),
+                                () -> Assertions.assertEquals(person.getLName(),"Khder","L Name  not Matches")
+                        ),
+                () -> Assertions.assertAll
+                        ("Person Phones",
+                                () -> Assertions.assertEquals(person.getPhone1(),"01113903660","Phone not Matches"),
+                                () -> Assertions.assertEquals(person.getPhone2(),"012555828555","Phone  not Matches")
+                        )
+               );
+    }
+}
+
+/*
+
+@Test
     void testing(){
         Student student = new Student();
         Assertions.assertAll
@@ -13,11 +40,6 @@ class StudentTest {
                     () -> Assertions.assertEquals(student.getAge(),"20","Age  not Matches")
                 );
     }
-
-}
-
-/*
-
     @BeforeEach
     void setUpFunction() {
         student = new Student();
