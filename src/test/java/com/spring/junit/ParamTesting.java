b85cece08a18265435b8a81cfdc9735f769536aa
Eslam Khder <eslamkhder81@gmail.com>
2021-07-21 05:28:50 +0200

    @CsvSource

--- src/test/java/com/spring/junit/ParamTesting.java
+++ src/test/java/com/spring/junit/ParamTesting.java
@@ -3,17 +3,40 @@ package com.spring.junit;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class ParamTesting {
 
 
+
     @DisplayName("Param Test")
     @ParameterizedTest(name = "{displayName}: {index} ===> {arguments}")
     //@ValueSource(strings = {"Eslam","Ahmed","Karim","Jo"})
     @EnumSource(Gender.class)
-    void myParamTesting(Gender value){
+    void myParamTesting1(Gender value){
         System.out.println(value);
     }
+    /*
+     * name  age  phone
+     * Ahmed 21   01235
+     * Karim 19   012254
+     * Jo   12    0125551
+     * */
+
+    @DisplayName("Param Test")
+    @ParameterizedTest(name = "{displayName}: {index} ===> {arguments}")
+    @CsvSource({
+            "Ahmed ,21, 01235",
+            "Karim ,19, 012254",
+            "Jo,12,0125551"
+    })
+    void myParamTesting2(String name,int age, String phone){
+        System.out.println("name: " + name);
+        System.out.println("age: " + age);
+        System.out.println("phone: " + phone);
+    }
+
+
 }
