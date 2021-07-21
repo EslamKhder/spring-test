2102fc1433ae206e63a608064899fccb35ead809
Eslam Khder <eslamkhder81@gmail.com>
2021-07-21 02:58:24 +0200

    inject parameters TestInfo && RepetitionInfo

--- src/test/java/com/spring/junit/StudentTest.java
+++ src/test/java/com/spring/junit/StudentTest.java
@@ -16,6 +16,17 @@ class StudentTest {
         System.out.println("HI");
     }
 
+    //@Test
+    @DisplayName("funRepeated Function")
+    @RepeatedTest(3)
+    void funRepeated(TestInfo testInfo,RepetitionInfo repetitionInfo){
+
+        System.out.println(testInfo.getDisplayName());
+        System.out.println(testInfo.getTestClass());
+        System.out.println(testInfo.getTestMethod());
+        System.out.println(testInfo.getTags());
+        System.out.println(repetitionInfo.getCurrentRepetition());
+    }
 }
 
 
